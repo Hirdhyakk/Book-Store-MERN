f3f5f3ee1665ca5accd3ce49b58db4d4f76c147b
Hirdhya Khanna <108194883+Hirdhyakk@users.noreply.github.com>
2024-10-08 10:16:17 +0530

    Update firebase.config.js

--- mern-client/src/firebase/firebase.config.js
+++ mern-client/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCnag5jM5z0acyPcreyGc626Dg5bpZrRbc",
+	apiKey: "",
 	authDomain: "mern-book-inventory-f4ab3.firebaseapp.com",
 	projectId: "mern-book-inventory-f4ab3",
 	storageBucket: "mern-book-inventory-f4ab3.appspot.com",
-	messagingSenderId: "74722224998",
-	appId: "1:74722224998:web:2c8d30cb5397e383faf6a7",
+	messagingSenderId: "",
+	appId: "",
 };
 
 // Initialize Firebase
