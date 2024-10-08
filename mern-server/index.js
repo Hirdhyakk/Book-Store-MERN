eb4015ae61d2eb33c7abf3344af50c1e1512742d
Hirdhya Khanna <108194883+Hirdhyakk@users.noreply.github.com>
2024-10-08 10:17:46 +0530

    Update index.js

--- mern-server/index.js
+++ mern-server/index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 // mongodb configuration
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-	"mongodb+srv://mern-book-store:C58yDNojQ7306WDu@cluster0.ikzbuzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+	"";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
